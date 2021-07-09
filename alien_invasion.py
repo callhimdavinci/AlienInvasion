81dd21c391da2d598e07dab756c578a18a900832
DESKTOP-J8RVEKS\Test <s.a.adei@student.tudelft.nl>
2021-07-09 22:56:51 +0200

    Making the fleet move.

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,31 +30,50 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _create_fleet(self):
         """"Create the fleet of aliens"""
         # make an alien
-        alien = Alien(self) #this is neccesary to extract the physical properties of an alien, like the width in the next line
+        alien = Alien(self)  # neccesary to extract the physical properties of an alien, like the width in the next line
         alien_width, alien_height = alien.rect.size
         ship_height = self.ship.image_rect.height
         available_space_x = self.settings.screen_width - (2 * alien_width)
-        available_space_y = self.settings.screen_height- (3 * alien_height) - ship_height
+        available_space_y = self.settings.screen_height - (3 * alien_height) - ship_height
         number_aliens_x = available_space_x // (2 * alien_width)
         number_aliens_y = available_space_y // (2*alien_width)
         for row_number in range(number_aliens_y):
             for alien_number in range(number_aliens_x):
                 self._create_alien(alien_number, row_number)
 
+    def _check_fleet_edges(self):
+        """Respond if any aliens reach the edge"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def _create_alien(self, alien_number, row_number):
         """"Create alien and place it in the row"""
         alien = Alien(self)
-        alien_width,alien_height = alien.rect.size
+        alien_width, alien_height = alien.rect.size
         alien.pos_x = alien_width + 2 * alien_width * alien_number
-        alien.pos_y =  alien_height +2 * alien_height*row_number
-        alien.rect.x,alien.rect.y = alien.pos_x, alien.pos_y
+        alien.pos_y = alien_height + 2 * alien_height*row_number
+        alien.rect.x, alien.rect.y = alien.pos_x, alien.pos_y
         self.aliens.add(alien)
 
+    def _update_aliens(self):
+        """"Check if the fleet is at an edge
+            Update the position of all aliens in the fleet"""
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _check_events(self):
         """"Respond to keypresses and mouse events"""
